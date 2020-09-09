65c2b2e1671da3930bb64036abe05f863f3e2492
yoshifumi.kubo <kubotutomu1028@gmail.com>
2020-09-09 12:05:05 +0000

    Revert "Add column"
    This reverts commit 342ac92ff23b87c2cf9da41690a3157a7436c285.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
   def index
     @tasks = Task.all
   end
   
   def show 
+    @task = Task.find(params[:id])
   end 
   
   def new
@@ -25,9 +24,12 @@ class TasksController < ApplicationController
   end 
   
   def edit
+    @task = Task.find(params[:id])
   end 
   
   def update
+    @task = Task.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -38,6 +40,7 @@ class TasksController < ApplicationController
   end 
   
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
@@ -46,12 +49,8 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content)
   end 
 end
